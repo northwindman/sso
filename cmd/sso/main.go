8c6a7ba83cdd6a1c24d69255689834f8e033562f
northwindman <nnorthwindmann@gmail.com>
2024-09-08 23:47:17 +0300

    Create logger and local pretty logger realisation

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,18 +1,64 @@
 package main
 
 import (
-	"fmt"
 	"github.com/northwindman/sso/internal/config"
+	"github.com/northwindman/sso/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("Start logging in this app!", slog.Int(
+		"Port", cfg.Port,
+	))
+
+	log.Debug("Start debug logging!")
 
 	// TODO: initial logger
 
 	// TODO: initial application
 
 	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log = &slog.Logger{}
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
